9be2b65a5e017ad6f6eb19b5aa5a9f47caee41df
Juliya Svetlakova <9268385539@mail.ru>
2023-05-31 09:49:45 +0300

    Main-added

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ public class Main {
     public static void main(String[] args) {
 
         int initialAccount = 100;
-        int refill = 1001;
+        int refill = 900;
         int oneBonus = 100;
+        int added = (initialAccount + refill);
         int bonus = (refill / oneBonus);
         int result = (initialAccount + refill + bonus);
 
@@ -12,7 +13,7 @@ public class Main {
             System.out.println(" Сумма на Вашем счете: " + result);
             System.out.println(" Зачисленно бонусных рублей: " + bonus);
         } else {
-            System.out.println(" Сумма на Вашем счете: " + result);
+            System.out.println(" Сумма на Вашем счете: " + added);
             System.out.println(" Бонусных рублей не зачислено ");
         }
     }
